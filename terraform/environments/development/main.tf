82db95134f65c7c14fa1258d1b365a1192fa21e8
aut0m4t0r <aut0m4t0r@outlook.com>
2024-04-07 20:55:02 -0400

    Added the ECS cluster and service to the terraform configuration in order to deploy the containerized vue app.

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -80,6 +80,25 @@ resource "aws_lb" "dev_alb_frontend" {
   }
 }
 
+resource "aws_lb_target_group" "dev_alb_tg" {
+  name     = "dev-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.dev_vpc.id
+  target_type = "ip"
+}
+
+resource "aws_lb_listener" "dev_alb_listener" {
+  load_balancer_arn = aws_lb.dev_alb_frontend.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.dev_alb_tg.arn
+  }
+}
+
 resource "aws_lb" "dev_alb_backend" {
   name               = "dev-alb-backend"
   internal           = true
@@ -179,3 +198,61 @@ resource "aws_security_group" "dev_alb_sg_backend" {
     Name = "dev-alb-sg-backend"
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_ecs_cluster" "dev_cluster" {
+  name = "dev-cluster"
+}
+
+resource "aws_ecs_task" "dev_frontend_task" {
+  family                   = "dev-frontend-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "dev-frontend"
+      image     = "${var.aws_account_id}.dkr.ecr.your-region.amazonaws.com/my-app:latest"
+      cpu       = 256
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+
+  cpu    = "256"
+  memory = "512"
+}
+
+resource "aws_ecs_service" "dev_frontend_service" {
+  name            = "dev-frontend-service"
+  cluster         = aws_ecs_cluster.dev_cluster.id
+  task_definition = aws_ecs_task_definition.dev_frontend_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = aws_subnet.dev_public_subnet[*].id
+    assign_public_ip = true
+    security_groups = [aws_security_group.dev_alb_sg_frontend.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.dev_alb_tg.arn
+    container_name   = "dev-frontend"
+    container_port   = 80
+  }
+
+  depends_on = [
+    aws_lb_listener.dev_alb_listener
+  ]
+}
+
